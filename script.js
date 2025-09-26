98cc9a8b9f1b8a64740a7c022d0d0a30929a2e1d
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-09-27 04:06:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+const list = document.getElementById('infi-list');
+let itemCount = 0;
 
+function addItems(n) {
+  for (let i = 0; i < n; i++) {
+    itemCount++;
+    const li = document.createElement('li');
+    li.textContent = `Item ${itemCount}`;
+    list.appendChild(li);
+  }
+}
+
+addItems(10);
+
+list.addEventListener('scroll', () => {
+	if (list.scrollTop + list.clientHeight >= list.scrollHeight - 1) {
+	    addItems(2);
+	}
+})
